1ae4088e345fdeb3ce8e49ff920884976e3c0d7d
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-02-19 11:52:09 +0000

    install hapi and start server

--- api/server.js
+++ api/server.js
@@ -1,20 +1,39 @@
-const express = require('express');
+'use strict';
+
+const Hapi = require('@hapi/hapi');
 const connectDb = require('./config/db');
 require('dotenv').config();
 require('colors');
 
-const PORT = process.env.PORT || 5200;
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 5200,
+    host: 'localhost',
+  });
 
-connectDb();
+  // LATER: Load plugins
+  // EXAMPLE: await server.register(require('hapi-auth-jwt2'));
 
-const app = express();
+  // LATER: Load routes
+  // EXAMPLE: await server.route(require('./routes'));
 
-app.listen(
-  PORT,
-  console.log('-----------------------------------------------'.yellow),
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-      .yellow
-  ),
-  console.log('-----------------------------------------------'.yellow)
-);
+  await server.start();
+  console.log('-----------------------------------------------'.yellow);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on ${server.info.uri}`.yellow);
+  console.log('-----------------------------------------------'.yellow);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+connectDb()
+  .then((db) => {
+    // Load routes and plugins, and start server
+    init();
+  })
+  .catch((err) => {
+    console.error(`Error connecting to MongoDB: ${err.message}`.red.underline.bold);
+    process.exit(1);
+  });
