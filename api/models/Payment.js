fb79da28441cfc826bc4b35792c4a2ce82ac9e5d
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-02-20 12:16:44 +0000

    Add user to Payment model

--- api/models/Payment.js
+++ api/models/Payment.js
@@ -5,6 +5,11 @@ const PaymentSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   space: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Space',
