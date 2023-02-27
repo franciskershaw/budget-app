d71aa187b69b188ffd65f71f78edea83b197d613
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-02-27 10:22:59 +0000

    Basic add token to cookie

--- client/src/hooks/auth/useUser.ts
+++ client/src/hooks/auth/useUser.ts
@@ -1,7 +1,29 @@
-export const setCookieToken = (token: string) => {
-  document.cookie = `token=${token}; Secure; SameSite=Strict;`;
-};
+import { useQueryClient, useQuery } from '@tanstack/react-query';
+import { queryKeys } from '../../reactQuery/queryKeys';
+import { User } from '../../types/types';
 
-export const clearCookieToken = () => {
+export function useUser() {
+  const queryClient = useQueryClient();
+
+	
+
+  // called from useAuth
+  function updateUser(newUser: User) {
+    queryClient.setQueryData([queryKeys.user], newUser);
+  }
+
+  function clearUser() {
+    queryClient.setQueryData([queryKeys.user], null);
+    clearCookieToken();
+  }
+
+  return { updateUser, clearUser };
+}
+
+export function setCookieToken(token: string) {
+  document.cookie = `token=${token}; Secure; SameSite=Strict;`;
+}
+
+export function clearCookieToken() {
   document.cookie = 'token=; Secure; SameSite=Strict; max-age=0;';
-};
+}
