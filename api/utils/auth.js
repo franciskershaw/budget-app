55e95246ff5239a03582d1802a86b62daafbc456
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-03-04 16:59:47 +0000

    remove commented code

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -32,24 +32,6 @@ const validate = async (decoded, request) => {
   }
 };
 
-// PREVIOUS VERSION
-// const validate = async (decoded, request, h) => {
-//   // Check if the user exists in the database
-//   const user = await User.findById(decoded._id);
-
-//   if (!user) {
-//     return { isValid: false };
-//   }
-
-//   // Check if the user has a valid access token
-//   const { isValid, credentials } = await request.server.auth.test('jwt', request);
-//   if (!isValid || !credentials || credentials._id !== decoded._id) {
-//     return { isValid: false };
-//   }
-
-//   return { isValid: true };
-// };
-
 const checkResourceUser = async (resourceId, userId, Model) => {
   const resource = await Model.findById(resourceId);
   return resource && resource.user.toString() === userId;
